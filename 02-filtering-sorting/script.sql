5f143c23d8d0fe0123ba4e2df160300cf37a34a4
mjmeilahn <mjmeilahn@gmail.com>
2021-11-06 13:49:41 -0700

    Update Scripts

--- 02-filtering-sorting/script.sql
+++ 02-filtering-sorting/script.sql
@@ -15,74 +15,78 @@ SELECT * FROM tbl_customers ORDER BY Country, City;
 
 SELECT * FROM tbl_customers HAVING COUNT(Purchases) > 2;
 
--- 5. Select where "Zipcode" is empty.
+-- 5. (Alias) Rename "Zipcode" as "Zip". Aliases can also be for TABLES.
+
+SELECT CustomerName, Zipcode AS Zip FROM tbl_customers;
+
+-- 6. Select where "Zipcode" is empty.
 
 SELECT * FROM tbl_customers WHERE Zipcode IS NULL;
 
--- 6. Select where "Zipcode" is not empty.
+-- 7. Select where "Zipcode" is not empty.
 
 SELECT * FROM tbl_customers WHERE Zipcode IS NOT NULL;
 
--- 7. Select where "City" starts w/ the letter "a".
+-- 8. Select where "City" starts w/ the letter "a".
 
 SELECT * FROM tbl_customers WHERE City LIKE 'a%';
 
--- 8. Select where "City" ends w/ the letter "a".
+-- 9. Select where "City" ends w/ the letter "a".
 
 SELECT * FROM tbl_customers WHERE City LIKE '%a';
 
--- 9. Select where "City" contains the letter "a".
+-- 10. Select where "City" contains the letter "a".
 
 SELECT * FROM tbl_customers WHERE City LIKE '%a%';
 
--- 10. Select where "City" begins w/ "a" and ends w/ "b".
+-- 11. Select where "City" begins w/ "a" and ends w/ "b".
 
 SELECT * FROM tbl_customers WHERE City LIKE 'a%b';
 
--- 11. Select where "City" does not begin w/ "a".
+-- 12. Select where "City" does not begin w/ "a".
 
 SELECT * FROM tbl_customers WHERE City NOT LIKE 'a%';
 
--- 12. (Wildcards) Select where 2nd letter is an "a".
+-- 13. (Wildcards) Select where 2nd letter is an "a".
 
 SELECT * FROM tbl_customers WHERE City LIKE '_a%';
 
--- 13. (Wildcards) Select where 1st letter is either "a, c, s".
+-- 14. (Wildcards) Select where 1st letter is either "a, c, s".
 
 SELECT * FROM tbl_customers WHERE City LIKE '[acs]%';
 
--- 14. (Wildcards) Select where 1st letter is ranged from "a to f".
+-- 15. (Wildcards) Select where 1st letter is ranged from "a to f".
 
 SELECT * FROM tbl_customers WHERE City LIKE '[a-f]%';
 
--- 15. (Wildcards) Select where 1st letter is not "a, c, f".
+-- 16. (Wildcards) Select where 1st letter is not "a, c, f".
 
 SELECT * FROM tbl_customers WHERE City LIKE '[!acf]%';
 
--- 16. Select where "Country" is either "USA" or "Japan".
+-- 17. Select where "Country" is either "USA" or "Japan".
 
 SELECT * FROM tbl_customers WHERE Country IN ('USA', 'Japan');
 
--- 17. Select where "Country" is not "USA" or "Japan".
+-- 18. Select where "Country" is not "USA" or "Japan".
 
 SELECT * FROM tbl_customers WHERE Country NOT IN ('USA', 'Japan');
 
--- 18. Select where "Price" is between 10 and 20.
+-- 19. Select where "Price" is between 10 and 20.
 
 SELECT * FROM tbl_products WHERE Price BETWEEN 10 AND 20;
 
--- 19. Select where "Price" is not between 10 and 20.
+-- 20. Select where "Price" is not between 10 and 20.
 
 SELECT * FROM tbl_products WHERE Price NOT BETWEEN 10 AND 20;
 
--- 20. Select where "ProductName" is alphabetically between "Blue Shirt" and "White Shirt"
+-- 21. Select where "ProductName" is alphabetically between "Blue Shirt" and "White Shirt"
 
 SELECT * FROM tbl_products WHERE ProductName BETWEEN 'Blue Shirt' AND 'White Shirt';
 
--- 21. Show the number of customers in each country.
+-- 22. Show the number of customers in each country.
 
 SELECT COUNT(CustomerID), Country FROM tbl_customers GROUP BY Country;
 
--- 22. Combine everything! Show customers by country, sort w/ highest customers first.
+-- 23. Combine everything! Show customers by country, sort w/ highest customers first.
 
 SELECT COUNT(CustomerID), Country FROM tbl_customers GROUP BY Country ORDER BY COUNT(CustomerID) DESC;
--- README.md
+++ README.md
@@ -3,6 +3,6 @@ A small repo of reusable and descriptive scripts for raw SQL.
 
 1. Basics - Select statements, asterisk (*) selector, columns, WHERE statements, AND/OR qualifiers, NOT qualifiers, DISTINCT to extract unique values in columns.
 
-2. Filtering & Sorting - Order By statements, Null qualifiers, RegEX patterns, Wildcards, Between & IN qualifiers and Groups. "WHERE" does not work with aggregate functions but "HAVING" does.
+2. Filtering & Sorting - Order By statements, Null qualifiers, RegEX patterns, Wildcards, Alias, Between & IN qualifiers and Groups. "WHERE" does not work with aggregate functions but "HAVING" does.
 
-3. Inserting & Updating - TBD
+3. Inserting & Updating - Standard data updates, manipulation and deletion.
