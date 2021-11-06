64d9df3506dea2e4eb2825a3f1d95b88db4baa8f
mjmeilahn <mjmeilahn@gmail.com>
2021-11-06 13:06:53 -0700

    Update Scripts

--- 02-filtering-sorting/script.sql
+++ 02-filtering-sorting/script.sql
@@ -11,74 +11,78 @@ SELECT * FROM tbl_customers ORDER BY City DESC;
 
 SELECT * FROM tbl_customers ORDER BY Country, City;
 
--- 4. Select where "Zipcode" is empty.
+-- 4. Select "tbl_customers" that have bought at least 3 times.
+
+SELECT * FROM tbl_customers HAVING COUNT(Purchases) > 2;
+
+-- 5. Select where "Zipcode" is empty.
 
 SELECT * FROM tbl_customers WHERE Zipcode IS NULL;
 
--- 5. Select where "Zipcode" is not empty.
+-- 6. Select where "Zipcode" is not empty.
 
 SELECT * FROM tbl_customers WHERE Zipcode IS NOT NULL;
 
--- 6. Select where "City" starts w/ the letter "a".
+-- 7. Select where "City" starts w/ the letter "a".
 
 SELECT * FROM tbl_customers WHERE City LIKE 'a%';
 
--- 7. Select where "City" ends w/ the letter "a".
+-- 8. Select where "City" ends w/ the letter "a".
 
 SELECT * FROM tbl_customers WHERE City LIKE '%a';
 
--- 8. Select where "City" contains the letter "a".
+-- 9. Select where "City" contains the letter "a".
 
 SELECT * FROM tbl_customers WHERE City LIKE '%a%';
 
--- 9. Select where "City" begins w/ "a" and ends w/ "b".
+-- 10. Select where "City" begins w/ "a" and ends w/ "b".
 
 SELECT * FROM tbl_customers WHERE City LIKE 'a%b';
 
--- 10. Select where "City" does not begin w/ "a".
+-- 11. Select where "City" does not begin w/ "a".
 
 SELECT * FROM tbl_customers WHERE City NOT LIKE 'a%';
 
--- 11. (Wildcards) Select where 2nd letter is an "a".
+-- 12. (Wildcards) Select where 2nd letter is an "a".
 
 SELECT * FROM tbl_customers WHERE City LIKE '_a%';
 
--- 12. (Wildcards) Select where 1st letter is either "a, c, s".
+-- 13. (Wildcards) Select where 1st letter is either "a, c, s".
 
 SELECT * FROM tbl_customers WHERE City LIKE '[acs]%';
 
--- 13. (Wildcards) Select where 1st letter is ranged from "a to f".
+-- 14. (Wildcards) Select where 1st letter is ranged from "a to f".
 
 SELECT * FROM tbl_customers WHERE City LIKE '[a-f]%';
 
--- 14. (Wildcards) Select where 1st letter is not "a, c, f".
+-- 15. (Wildcards) Select where 1st letter is not "a, c, f".
 
 SELECT * FROM tbl_customers WHERE City LIKE '[!acf]%';
 
--- 15. Select where "Country" is either "USA" or "Japan".
+-- 16. Select where "Country" is either "USA" or "Japan".
 
 SELECT * FROM tbl_customers WHERE Country IN ('USA', 'Japan');
 
--- 16. Select where "Country" is not "USA" or "Japan".
+-- 17. Select where "Country" is not "USA" or "Japan".
 
 SELECT * FROM tbl_customers WHERE Country NOT IN ('USA', 'Japan');
 
--- 17. Select where "Price" is between 10 and 20.
+-- 18. Select where "Price" is between 10 and 20.
 
 SELECT * FROM tbl_products WHERE Price BETWEEN 10 AND 20;
 
--- 17. Select where "Price" is not between 10 and 20.
+-- 19. Select where "Price" is not between 10 and 20.
 
 SELECT * FROM tbl_products WHERE Price NOT BETWEEN 10 AND 20;
 
--- 18. Select where "ProductName" is alphabetically between "Blue Shirt" and "White Shirt"
+-- 20. Select where "ProductName" is alphabetically between "Blue Shirt" and "White Shirt"
 
 SELECT * FROM tbl_products WHERE ProductName BETWEEN 'Blue Shirt' AND 'White Shirt';
 
--- 19. Show the number of customers in each country.
+-- 21. Show the number of customers in each country.
 
 SELECT COUNT(CustomerID), Country FROM tbl_customers GROUP BY Country;
 
--- 20. Combine everything! Show customers by country, sort w/ highest customers first.
+-- 22. Combine everything! Show customers by country, sort w/ highest customers first.
 
 SELECT COUNT(CustomerID), Country FROM tbl_customers GROUP BY Country ORDER BY COUNT(CustomerID) DESC;
