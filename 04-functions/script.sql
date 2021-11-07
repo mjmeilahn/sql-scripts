ae624ff2e373d2cd53bc10d4d373f98328d6569e
mjmeilahn <mjmeilahn@gmail.com>
2021-11-07 13:40:18 -0800

    Update script

--- 04-functions/script.sql
+++ 04-functions/script.sql
@@ -1,20 +1,20 @@
 
--- 1. Use the MIN() function to select a record w/ the smallest price.
+-- 1. (Numbers) Use the MIN() function to select a record w/ the smallest price.
 
 SELECT MIN(Price) from tbl_products;
 
--- 2. Use the MAX() function to select a record w/ the highest price.
+-- 2. (Numbers) Use the MAX() function to select a record w/ the highest price.
 
 SELECT MAX(Price) from tbl_products;
 
--- 3. Show how many records have a Price set to 23.
+-- 3. (Numbers) Show how many records have a Price set to 23.
 
 SELECT COUNT(*) FROM tbl_products WHERE Price = 23;
 
--- 4. Calculate the averge price of products.
+-- 4. (Numbers) Calculate the averge price of products.
 
 SELECT AVG(Price) FROM tbl_products;
 
--- 5. Calculate the full amount of all product prices.
+-- 5. (Numbers) Calculate the full amount of all product prices.
 
 SELECT SUM(Price) FROM tbl_products;
