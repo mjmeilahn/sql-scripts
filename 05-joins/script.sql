78c7e5cf0070dea591a932469a65f09275632b20
mjmeilahn <mjmeilahn@gmail.com>
2021-11-08 13:15:20 -0800

    Finish Join Scripts

--- 05-joins/script.sql
+++ 05-joins/script.sql
@@ -6,3 +6,7 @@ SELECT * FROM tbl_orders LEFT JOIN tbl_customers ON tbl_orders.CustomerID = tbl_
 -- 2. Now repeat this in an inner join.
 
 SELECT * FROM tbl_orders INNER JOIN tbl_customers ON tbl_orders.CustomerId = tbl_customers.CustomerID;
+
+-- 3. Now repeat this in a right join.
+
+SELECT * FROM tbl_orders RIGHT JOIN tbl_customers ON tbl_orders.CustomerId = tbl_customers.CustomerID;
